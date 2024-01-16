fa4ca4d413f93e81d09b99f2642f7f8b46d2a0dd
Diegojadrian97 <diegojadrian97@gmail.com>
2024-01-16 13:02:15 -0300

    agregando frontend url

--- Backend/src/config.js
+++ Backend/src/config.js
@@ -1,3 +1,5 @@
+import "dotenv/config.js";
+
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 export const PORT = process.env.PORT || 3000;
 export const MY_EMAIL = process.env.MY_EMAIL || "diegojadrian97@gmail.com";
